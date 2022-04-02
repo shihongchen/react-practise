d819e96b16c2595f1cb14626ea21846d9f6c7cf0
shihongchen <88976891+shihongchen@users.noreply.github.com>
2022-04-03 01:15:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import DisplayPage from './page/DisplayPage'
+import HandlePage from './page/HandlePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            num: 0
+        }
+    }
+
+    add = () => {
+        let { num } = this.state;
+        num++;
+        this.setState({ num })
+    }
+
+    reduce = () => {
+        let { num } = this.state;
+        num--;
+        this.setState({ num })
+    }
+
+    render() {
+        const { num } = this.state;
+        return (
+            <div>
+                <HandlePage add={this.add} reduce={this.reduce} />
+                <DisplayPage num={num} />
+            </div>
+        )
+    }
+    
+
+    // handleClick=(e) => {
+    //     console.log(e.target.value)
+    //     this.setState({
+    //         num: e.target.value
+    //     })
+    // }
+    // render() {
+    //     const val = this.state.num
+    //     return (
+    //         <div>
+    //             <h3>{ val }</h3>
+    //             <button 
+    //             value={ Number(val) + 1 }
+    //             onClick={ this.handleClick } >+</button>
+
+    //             <button 
+    //             value={ Number(val) - 1 }
+    //             onClick={ this.handleClick } >—</button>
+    //         </div>
+    //     )
+    // }
 }
-
-export default App;
